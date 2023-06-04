8ffa82c82b3487191c8267641830c482861faf61
Dhanushbharanika <119720388+Dhanushbharanika@users.noreply.github.com>
2023-06-04 12:41:19 +0530

    import java.util.*; public class codemind {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         System.out.println(((a)*(a-1))/2);         sc.close();             }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class codemind
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in); 
+        int a;
+        a=sc.nextInt();
+        System.out.println(((a)*(a-1))/2);
+        sc.close(); 
+            }
+    
+}
