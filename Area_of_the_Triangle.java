7bd0936ea63b1659be7f3a7f480b0dda08dcd82e
Dhanushbharanika <119720388+Dhanushbharanika@users.noreply.github.com>
2023-06-04 11:32:33 +0530

    import java.util.Scanner; public class codemind {     public static void main(String args[])     {         int a,b,c; float s,n;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         s=(float)(a+b+c)/2; n=s*(s-a)*(s-b)*(s-c);         System.out.printf("%.2f",Math.sqrt(n));         sc.close();         }
    }

--- Area_of_the_Triangle.java
+++ Area_of_the_Triangle.java
@@ -1,1 +1,16 @@
-import java.util.Scanner; public class Sample{ public static void main(String args[])throws Exception { int a,b,c; float s,area; Scanner sc=new Scanner(System.in); a=sc.nextInt(); b=sc.nextInt(); c=sc.nextInt(); s=(float)(a+b+c)/2; area=(float)Math.sqrt(s*(s-a)*(s-b)*(s-c)); System.out.format("%.2f",area); }}
+import java.util.Scanner;
+public class codemind
+{
+    public static void main(String args[])
+    {
+        int a,b,c; float s,n;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        s=(float)(a+b+c)/2; n=s*(s-a)*(s-b)*(s-c);
+        System.out.printf("%.2f",Math.sqrt(n));
+        sc.close();
+        }
+    
+}
